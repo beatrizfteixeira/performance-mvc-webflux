ba42d809c24c863c29b9d57a0d8348ee2524ef63
beatrizfteixeira <beatriz.teixeira@estudante.ufla.br>
2025-09-29 22:56:32 -0300

    formatting and renaming files

--- mvc.project/src/main/java/com/biaft/mvc/roject/dto/DataDto.java
+++ mvc.project/src/main/java/com/biaft/mvc/roject/dto/DataDto.java
@@ -1,19 +1,19 @@
 package com.biaft.mvc.roject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DataDto {
     private int id;
     private String name;
     private long timestamp;
-
-    public DataDto() {
-    }
 
     public DataDto(int id, String name) {
         this.id = id;
